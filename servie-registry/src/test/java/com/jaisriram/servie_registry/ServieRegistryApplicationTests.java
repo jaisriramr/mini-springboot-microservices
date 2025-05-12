5f309e00a066d44fe00a12bc7f067ebc03f21a76
jaisriram <jaisriram.webdeveloper@gmail.com>
2025-05-12 23:51:08 +0530

    init

---
+++ servie-registry/src/test/java/com/jaisriram/servie_registry/ServieRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaisriram.servie_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServieRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
